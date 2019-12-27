330678e160e06a28c68dabf83c15e74324771b07
EpifSergey <epif-sergey@yandex.ru>
2019-12-27 15:40:16 +0300

    ping server

--- mocserver.py
+++ mocserver.py
@@ -1,11 +1,17 @@
 import http.server
+import json
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+        if self.path == '/ping':
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(json.dumps({'ping':'ok'}).encode())
+            return
+        http.server.SimpleHTTPRequestHandler.do_GET(self)
 
-class MocServer(http.server.HTTPServer):
+if __name__ == "__main__":
     port = 80
     address = '127.8.8.8'
-    def __init__(self):
-        self.httpd = http.server.HTTPServer((self.address, self.port), MyHandler)
+    httpd = http.server.HTTPServer((address, port), MyHandler)
+    httpd.serve_forever()
--- test_server.py
+++ test_server.py
@@ -1,10 +1,20 @@
 import unittest
-import mocserver
+import requests
+import time
+import json
 
-class TestMocServer(unittest.TestCase):
-    def test_create_server(self):
-        server = mocserver.MocServer()
-        self.assertNotEqual(server, None)
+url = 'http://magic.troll.me/'
+
+class TestMocHandler(unittest.TestCase):
+    def test_doget_check_ping(self):
+        response = requests.get(url + 'ping')
+        try:
+            js = json.loads(response.text)
+            self.assertEqual(js['ping'], 'ok')
+        except:
+            self.assertTrue(False)
+        
 
 if __name__ == "__main__":
+    
     unittest.main()
