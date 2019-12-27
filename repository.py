2bd0b6bee7420fe8c8b6e03c19ec40b29462526e
EpifSergey <epif-sergey@yandex.ru>
2019-12-27 18:39:11 +0300

    get friends

--- repository.py
+++ repository.py
@@ -6,13 +6,36 @@ class Repository:
         self.url = url
         self.port = port
 
-    def get(self, id):
-        pass
+    def get(self, rurl):
+        response = requests.get(rurl)
+        try:
+            js = json.loads(response.text)
+            return js
+        except:
+            return None
 
 class UsersRepository(Repository):
-    def get(self, id):
-        pass
+    def getUserFriends(self, user_id, token):
+        rurl = self.url + 'method/friends.get?user_id=' + str(user_id) + '&v=5.61&access_token=' + token
+        res = self.get(rurl)
+        try:
+            return res['response']['items']
+        except:
+            return None
+    
+    def getUserGroups(self, user_id, token):
+        rurl = self.url + 'method/groups.get?user_id=' + str(user_id) + '&v=5.61&access_token=' + token
+        res = self.get(rurl)
+        try:
+            return res['response']['items']
+        except:
+            return None
 
 class GroupsRepository(Repository):
-    def get(self, id):
-        pass
+    def getGroup(self, group_id, token):
+        rurl = self.url + 'method/groups.getById?user_id=' + str(group_id) + '&v=5.61&access_token=' + token
+        res = self.get(rurl)
+        try:
+            return res['response']
+        except:
+            return None
--- test_repository.py
+++ test_repository.py
@@ -9,6 +9,15 @@ class TestRepositories(unittest.TestCase):
     def test_users_repository(self):
         rep = repository.UsersRepository(url)
         self.assertNotEqual(rep, None)
+    
+    def test_groups_repository(self):
+        rep = repository.GroupsRepository(url)
+        self.assertNotEqual(rep, None)
+    
+    def test_get_friends(self):
+        rep = repository.UsersRepository(url)
+        res = rep.getUserFriends(1, token)
+        self.assertEqual(res, [1, 2, 3])
 
 if __name__ == "__main__":
     unittest.main()
